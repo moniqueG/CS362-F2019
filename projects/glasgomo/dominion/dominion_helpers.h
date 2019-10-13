ceb78e7be70925fbc8bf102e3c6c60840434414c
tweedley <monique.glasgow@gmail.com>
2019-10-13 08:47:31 -0400

    Added files for assignment 2

--- projects/glasgomo/dominion/dominion_helpers.h
+++ projects/glasgomo/dominion/dominion_helpers.h
@@ -4,6 +4,7 @@
 #include "dominion.h"
 
 int drawCard(int player, struct gameState *state);
+int drawCards(int player, struct gameState *state, int numOfCards);
 int updateCoins(int player, struct gameState *state, int bonus);
 int discardCard(int handPos, int currentPlayer, struct gameState *state,
                 int trashFlag);
@@ -11,5 +12,11 @@ int gainCard(int supplyPos, struct gameState *state, int toFlag, int player);
 int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3,
                struct gameState *state, int handPos, int *bonus);
+int cardEffectAmbassador(int choice1, int choice2, struct gameState *state, int handPos);
+int cardEffectBaron(int choice1, struct gameState *state);
+int cardEffectMinion(int choice1, int choice2, struct gameState *state, int handPos);
+int cardEffectTribute(struct gameState *state);
+int cardEffectMine(int choice1, int choice2, struct gameState *state, int handPos);
+
 
 #endif
